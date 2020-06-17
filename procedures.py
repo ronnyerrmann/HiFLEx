6bb9c0e75db527e5cc1e465b932bf7602709fff5
Ronny Errmann <ronny.errmann@gmail.com>
2020-06-17 15:01:07 +0100

    Improvements

--- procedures.py
+++ procedures.py
@@ -2217,7 +2217,7 @@ def adjust_trace_order(kwargs):
             oldcenter = trace_pos[order+1,j]                        # +1 because first entry are pixels
             if oldcenter > -maxFWHM:                                # center is expected to be in the image
                 cen_gauss, cen_poly, width, leftmin,rightmin = find_center(im[i,:], int(round(oldcenter)), i, maxFWHM, border_pctl=params['width_percentile'], significance=3.0, polymax=True)
-                logger('{0} {1} {2} {3}'.format(order,j,cen_gauss,cen_poly), show=False, printarrayformat=[], printarray=[], logfile='gauss_vs_poly.txt')
+                #logger('{0} {1} {2} {3}'.format(order,j,cen_gauss,cen_poly), show=False, printarrayformat=[], printarray=[], logfile='gauss_vs_poly.txt')
                 center = cen_poly
             else:
                 center, width, lastadd = 0,0, i
@@ -7732,16 +7732,16 @@ def header_results_to_texfile(params, header_keywords=[]):
         header_keywords.append(['HIERARCH HiFLEx JD',           'JD at middle of exposure',     '[d]'])
         header_keywords.append(['HIERARCH HiFLEx fsum_all',     'Total extracted flux',         '[ADU]'])
         header_keywords.append(['HIERARCH HiFLEx BCKNOISE',     'Background noise',             '[ADU]'])
-        header_keywords.append(['HIERARCH HiFLEx CD_SHIFT',     'Shift in Cross-Dispersion',    '[px]'])
-        header_keywords.append(['HIERARCH HiFLEx CD_S_WDTH',    'Width of the shift in CD',     '[px]'])
-        header_keywords.append(['HIERARCH HiFLEx D_SHIFT',      'Shift in Dispersion',          '[px]'])
-        header_keywords.append(['HIERARCH HiFLEx D_SHIFT_ERR',  'Uncertainty of the shift in D','[px]'])
+        header_keywords.append(['HIERARCH HiFLEx CD_SHIFT',     'Offset in Cross-Dispersion',   '[px]'])
+        header_keywords.append(['HIERARCH HiFLEx CD_S_WDTH',    'Width of the offset in CD',    '[px]'])
+        header_keywords.append(['HIERARCH HiFLEx D_SHIFT',      'Offset in Dispersion (incldung DT)',           '[px]'])
+        header_keywords.append(['HIERARCH HiFLEx D_SHIFT_ERR',  'Uncertainty of the offset in D',               '[px]'])
         header_keywords.append(['HIERARCH HiFLEx D_SHIFT_NUMBER_LINES', 'Number of lines used to calculate the shift', ''])
-        header_keywords.append(['HIERARCH HiFLEx D_WIDTH',      'Gaussian width of the calibration lines',         '[px]'])
-        header_keywords.append(['HIERARCH HiFLEx D_SHIFT_KMS',  'Shift in Dispersion',                             '[km/s]'])
-        header_keywords.append(['HIERARCH HiFLEx DT_SHIFT',     'Shift between science and calibration fiber',     '[px]'])
-        header_keywords.append(['HIERARCH HiFLEx DT_SHIFT_KMS', 'Shift between science and calibration fiber',     '[km/s]'])
-        header_keywords.append(['HIERARCH HiFLEx BJDTDB',       'Barycentric correct JD (incl. leap seconds)',     '[d]'])
+        header_keywords.append(['HIERARCH HiFLEx D_WIDTH',      'Gaussian width of the calibration lines',      '[px]'])
+        header_keywords.append(['HIERARCH HiFLEx D_SHIFT_KMS',  'Offset in Dispersion',                         '[km/s]'])
+        header_keywords.append(['HIERARCH HiFLEx DT_SHIFT',     'Offset between wavelength solutions',          '[px]'])
+        header_keywords.append(['HIERARCH HiFLEx DT_SHIFT_KMS', 'Offset between wavelength solutions',          '[km/s]'])
+        header_keywords.append(['HIERARCH HiFLEx BJDTDB',       'Barycentric correct JD (incl. leap seconds)',  '[d]'])
         header_keywords.append(['HIERARCH HiFLEx CERES RV',     'CERES RV (not corrected for BVC)', '[km/s]'])
         header_keywords.append(['HIERARCH HiFLEx CERES RV_BARY','CERES RV (corrected for BVC)', '[km/s]'])
         header_keywords.append(['HIERARCH HiFLEx CERES RV_ERR', 'CERES RV error',               '[km/s]'])
