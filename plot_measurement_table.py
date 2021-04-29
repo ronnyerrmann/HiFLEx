26e0b9001b65b2f16f9882527e564c6449640cda
Ronny Errmann <ronny.errmann@gmail.com>
2021-04-29 13:48:45 +0100

    small improvements

--- plot_measurement_table.py
+++ plot_measurement_table.py
@@ -2,11 +2,13 @@
 import sys
 sys.path.append('/home/ronny/Scripts/hiflex/')
 from procedures import *
+import csv
 
 params = dict()
 
 savefile = 'measurement_table.pdf'
 datafile = 'measurement_table.csv'      # assumes that the objectnames are somewhat in column one or two
+sensorsfile = 'TSP01_20210424-1420.csv'      # optional
 objectfile = 'objects_reduced.lst'
 x_range = None
 size_inches = [16.2, 10]
@@ -15,9 +17,6 @@ objects += read_text_file(objectfile, no_empty_lines=False)
 if len(objects) == 1:
     # Code defined what objects to use
     objects = ['*', 'Sun', ['alpOri','alphori','alfori','alphaori'], 'AlphaAri', 'alpcmi', ['gamVirA','gamVir2'], ['gamVirB','gamVir1'], 'TauBoo', 'Tung' ]
-
-if datafile.find(os.sep) == -1:
-    datafile = os.getcwd() + os.sep + datafile      # To select settings depending on datafile
 
 select = []     
 if True:
@@ -36,6 +35,11 @@ if True:
     selectd.append([ 'data', 5, 12, None, '', 'k', '', 'o', 2, '', 'Number' ])
     select.append(selectd)
     selectd = []
+    selectd.append([ 'sensordata', 0, 1, None, 'Room - Wall', 'b', '', 'o', 2, '', '' ])
+    selectd.append([ 'sensordata', 0, 2, None, 'Room - front right', 'r', '', 'o', 2, 'JD [d]', 'Temperature' ])
+    #selectd.append([ 'text', 294, 40, 'Helium 1 l/min', 'k', 90, 'left', 'bottom' ])
+    select.append(selectd)
+    selectd = []
     selectd.append([ 'data', 5, 6, None, '', 'k', '', 'o', 2, '', 'Flux (ADU)' ])
     select.append(selectd)
     selectd = []
@@ -51,6 +55,19 @@ if True:
     select.append(selectd)
     #x_range = [-10, 470]
     #size_inches = [10, 6]
+
+if datafile.find(os.sep) == -1:
+    datafile = os.getcwd() + os.sep + datafile      # To select settings depending on datafile
+
+if os.path.isfile(sensorsfile):
+    with open(sensorsfile, 'r') as f:
+        header = list(csv.reader(f, delimiter=','))
+        data = np.array(header[1:])
+        times1 = [ get_julian_datetime( datetime.datetime.strptime(dat, '%Y%m%d-%H%M%S') ) for dat in data[:,0]]
+        times2 = [ get_julian_datetime( datetime.datetime.strptime(dat, '%Y%m%d-%H%M%S') ) for dat in data[:,-1]]
+        sensordata = np.vstack((times1, data[:,1:-1].astype(float).T, times2)).T
+else:
+    sensordata = np.array([])
 
 data = read_text_file(datafile, no_empty_lines=False)
 len_entries = len( data[0].split('\t') )
@@ -92,8 +109,10 @@ with PdfPages(savefile) as pdf:
                 frame_ii_g.set_title('{0}'.format(star))
             x_title, y_title, y_range = '', '', None
             for ii_d in range(nr_d):
-                if selectd[ii_d][0] == 'data':
-                    [indexx, indexy, indexe, label, color, linestyle, marker, markersize ] = selectd[ii_d][1:9]
+                if selectd[ii_d][0] in ['data','sensordata']:
+                    [dset, indexx, indexy, indexe, label, color, linestyle, marker, markersize ] = selectd[ii_d][0:9]
+                    if dset == 'sensordata':
+                        x_range_tmp = copy.copy(frame_ii_g.get_xlim())
                     if indexy < 0:
                         indexy = abs(indexy)
                         sign = -1
@@ -101,31 +120,39 @@ with PdfPages(savefile) as pdf:
                         sign = 1
                     for index in [indexx, indexy, indexe]:
                         if index is None:   continue
-                        if '{0}'.format(index) not in converted:
-                            converted['{0}'.format(index)] = data[index,:].astype(float)
+                        if '{0}_{1}'.format(index, dset) not in converted.keys():
+                            if dset == 'data':
+                                converted['{0}_{1}'.format(index, dset)] = data[index,:].astype(float)
+                            elif dset == 'sensordata':
+                                converted['{0}_{1}'.format(index, dset)] = sensordata[:,index]
                     #x_title = header[1,indexx]+' '+header[2,indexx]
                     #if x_title not in x_titles:
                     #    x_titles.append(x_title)
-                    if label == '':
+                    if label == '' and dset == 'data':
                         label = header[indexy, 1]
                         if len(label) > 25:
                             for ii in list(range(25, 18, -1)) + list(range(25,min(len(label),35))):
                                 if label[ii] == ' ':
                                     label = label[:ii] + '\n' + label[ii+1:]
                                     break
-                    good_data = ~np.isnan(converted['{0}'.format(indexx)]) & ~np.isnan(converted['{0}'.format(indexy)]) & starindata
+                    if dset == 'data':
+                        good_data = ~np.isnan(converted['{0}_{1}'.format(indexx, dset)]) & ~np.isnan(converted['{0}_{1}'.format(indexy, dset)]) & starindata
+                    else:
+                        good_data = np.ones_like(converted['{0}_{1}'.format(indexx, dset)], dtype=bool)
                     if np.sum(good_data) > 0:       # Only plot if data is available
-                        xx = converted['{0}'.format(indexx)][good_data]
-                        yy = sign*converted['{0}'.format(indexy)][good_data]
+                        xx = converted['{0}_{1}'.format(indexx, dset)][good_data]
+                        yy = sign*converted['{0}_{1}'.format(indexy, dset)][good_data]
                         if y_range is not None:
                             yy[ yy > y_range[1] ] = y_range[1]
                             yy[ yy < y_range[0] ] = y_range[0]
                         if indexe is None:          # No error bars
                             frame_ii_g.plot(xx, yy, label=label, color=color, linestyle=linestyle, marker=marker, markersize=markersize)
                         else:                       # error bars
-                            frame_ii_g.errorbar(xx, yy, yerr=converted['{0}'.format(indexe)][good_data], label=label, color=color, linestyle=linestyle, marker=marker, markersize=markersize)
+                            frame_ii_g.errorbar(xx, yy, yerr=converted['{0}_{1}'.format(indexe, dset)][good_data], label=label, color=color, linestyle=linestyle, marker=marker, markersize=markersize)
                     if len(selectd[ii_d]) == 11:
                         [x_title, y_title] = selectd[ii_d][9:11]
+                    if dset == 'sensordata':
+                        frame_ii_g.set_xlim(x_range_tmp[0],x_range_tmp[1])
                 elif selectd[ii_d][0] == 'text':
                     [posx, posy, text, color, rotation, hpos, vpos ] = selectd[ii_d][1:8]
                     frame_ii_g.text( posx, posy, text, horizontalalignment=hpos, verticalalignment=vpos, rotation=rotation, color=color, zorder=5 )
